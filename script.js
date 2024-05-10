51abc6ec0cacf51291b4b18d35c5d9b07f451139
Hlelo <127110080+hlelo101@users.noreply.github.com>
2024-05-10 15:21:18 +0200

    Add JS for exit button & comments
    Add the code to make the exit button work and add comments for readability

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
+// Alert
 var alertButton = document.getElementById('alertButton');
+// Redirect
 var redirectToButton = document.getElementById('usefulWebsite');
 var redirectTo = document.getElementById('redirectTo');
 var redirectToInput = document.getElementById('redirectToInput');
 var redirectButton = document.getElementById('redirectButton');
+// Exit
+var closeWebsite = document.getElementById('closeWebsite');
+var exitConfirmWindow = document.getElementById('exitConfirm');
+var exitNo = document.getElementById('exitNo');
+// Hide by default
 redirectTo.style.display = 'none';
-
+exitConfirmWindow.style.display = 'none';
+// Alert
 alertButton.addEventListener('click', function() {
     alert('This is an alert');
 });
+// Redirect
 redirectToButton.addEventListener('click', function() {
     if (redirectTo.style.display === 'none') {
         redirectTo.style.display = 'block';
@@ -21,4 +30,18 @@ redirectButton.addEventListener('click', function() {
         url = 'http://' + url;
     }
     window.location.href = url;
+});
+// Exit
+closeWebsite.addEventListener('click', function() {
+    if (exitConfirmWindow.style.display === 'none') {
+        exitConfirmWindow.style.display = 'block';
+    } else {
+        exitConfirmWindow.style.display = 'none';
+    }
+});
+exitNo.addEventListener('click', function() {
+    exitConfirmWindow.style.display = 'none';
+});
+exitYes.addEventListener('click', function() {
+    window.close();
 });
