0f336ae0b3f5d50f4f5ec3f6e35cccfa9b3e0b22
Hlelo <127110080+hlelo101@users.noreply.github.com>
2024-06-15 16:22:54 +0200

    New way of detecting if browser is Firefox
    Used another way of detecting if the user's browser is Firefox (for the "system info" in the toolbar) to avoid using "typeof installtrigger" that will be deprecated and removed.

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ function getSystemInfo() {
     // Opera 8.0+
     var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
     // Firefox 1.0+
-    var isFirefox = typeof InstallTrigger !== 'undefined';
+    var isFirefox = /Firefox[\/\s](\d+\.\d+)/.test(navigator.userAgent);
     // Safari 3.0+ "[object HTMLElementConstructor]" 
     var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && window['safari'].pushNotification));
     // Internet Explorer 6-11
